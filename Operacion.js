6afb30c72e97385abc96adcb149ac2209d132302
Paula <soypaulaespitia@gmail.com>
2025-06-28 18:45:03 -0500

    Operaciones Basicas

---
+++ Operacion.js
@@ -0,0 +1,27 @@
+function suma(){
+    let n1 =(document.getElementById("numero1").value);
+    let n2 =(document.getElementById("numero2").value);
+    let suma = n1+n2;
+    document.getElementById("Resultado").value = suma;
+}
+
+function resta(){
+    let n1 =(document.getElementById("numero1").value);
+    let n2 =(document.getElementById("numero2").value);
+    let resta = n1-n2;
+    document.getElementById("Resultado").value = resta;
+}
+
+function division(){
+    let n1 =(document.getElementById("numero1").value);
+    let n2 =(document.getElementById("numero2").value);
+    let division = n1/n2;
+    document.getElementById("Resultado").value = division;
+}
+
+function multiplicacion(){
+    let n1 =(document.getElementById("numero1").value);
+    let n2 =(document.getElementById("numero2").value);
+    let multiplicacion = n1*n2;
+    document.getElementById("Resultado").value = multiplicacion;
+}
