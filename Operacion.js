d6e4f8cfc7f977144cb00b75f38bf8ebdeb83fd6
Paula <soypaulaespitia@gmail.com>
2025-06-28 21:01:46 -0500

    parseInt

--- Operacion.js
+++ Operacion.js
@@ -1,27 +1,28 @@
 function suma(){
-    let n1 =(document.getElementById("numero1").value);
-    let n2 =(document.getElementById("numero2").value);
-    let suma = n1+n2;
+    let n1 =parseInt(document.getElementById("numero1").value);
+    let n2 =parseInt(document.getElementById("numero2").value);
+    let suma = n1 + n2;
     document.getElementById("Resultado").value = suma;
+    
 }
 
 function resta(){
-    let n1 =(document.getElementById("numero1").value);
-    let n2 =(document.getElementById("numero2").value);
+    let n1 =parseInt(document.getElementById("numero1").value);
+    let n2 =parseInt(document.getElementById("numero2").value);
     let resta = n1-n2;
     document.getElementById("Resultado").value = resta;
 }
 
 function division(){
-    let n1 =(document.getElementById("numero1").value);
-    let n2 =(document.getElementById("numero2").value);
+    let n1 =parseInt(document.getElementById("numero1").value);
+    let n2 =parseInt(document.getElementById("numero2").value);
     let division = n1/n2;
     document.getElementById("Resultado").value = division;
 }
 
 function multiplicacion(){
-    let n1 =(document.getElementById("numero1").value);
-    let n2 =(document.getElementById("numero2").value);
+    let n1 =parseInt(document.getElementById("numero1").value);
+    let n2 =parseInt(document.getElementById("numero2").value);
     let multiplicacion = n1*n2;
     document.getElementById("Resultado").value = multiplicacion;
 }
